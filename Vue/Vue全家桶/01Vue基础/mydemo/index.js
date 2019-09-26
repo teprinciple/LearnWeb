f149ad4132a1e5c22c1357e59e2648794b93c54f
teprinciple <teprinciple@foxmail.com>
2019-09-26 22:53:06 +0800

    vue

---
+++ Vue/Vue全家桶/01Vue基础/mydemo/index.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+
+    new Vue({
+        el:"#app",
+        data:{
+            tabs: ["tab1","tab2","tab3","tab4"]
+
+        },
+        methods:{
+
+        },
+        mounted:{ // 挂载完成
+
+        }
+    })
+}
