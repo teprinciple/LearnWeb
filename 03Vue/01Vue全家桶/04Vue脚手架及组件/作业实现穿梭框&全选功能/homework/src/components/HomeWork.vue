8f7a17ec59e398f85086ec302ba9e09b70643f8d
Teprinciple <teprinciple@foxmail.com>
2019-10-11 15:10:26 +0800

    我的作业

---
+++ 03Vue/01Vue全家桶/04Vue脚手架及组件/作业实现穿梭框&全选功能/homework/src/components/HomeWork.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    这是我的作业1
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'HomeWork',
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+
+  },
+}
+</script>
+
+
+<style scoped>
+
+*{
+  margin: 0;
+  padding: 0;
+}
+
+</style>
---
+++ 03Vue/01Vue全家桶/04Vue脚手架及组件/作业实现穿梭框&全选功能/homework/src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HomeWork from '@/components/HomeWork'
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [{
+        path: '/',
+        name: 'HomeWork',
+        component: HomeWork
+    }]
+})
