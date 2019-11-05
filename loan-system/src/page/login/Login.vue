62bf96347e8618f060838e0639b7d723c9d23de7
teprinciple <teprinciple@foxmail.com>
2019-11-05 22:44:30 +0800

    loan-system

---
+++ loan-system/src/page/login/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main">登录</div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+$white-color: red;
+
+.main {
+  color: $white-color;
+}
+</style>
